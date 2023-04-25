419e3d957beba081bfbd1ea044ce5ef7e19b7afe
Marc Rambow <marc.rambow@sinc.de>
2023-04-25 20:47:21 +0200

    adding camera test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera } from '@capacitor/camera';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -6,15 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'stepper', url: '/stepper', icon: 'mail' },
-    { title: 'modals', url: '/modals', icon: 'mail' },
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    {title: 'stepper', url: '/stepper', icon: 'mail'},
+    {title: 'modals', url: '/modals', icon: 'mail'},
+    {title: 'Inbox', url: '/folder/Inbox', icon: 'mail'},
+    {title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane'},
+    {title: 'Favorites', url: '/folder/Favorites', icon: 'heart'},
+    {title: 'Archived', url: '/folder/Archived', icon: 'archive'},
+    {title: 'Trash', url: '/folder/Trash', icon: 'trash'},
+    {title: 'Spam', url: '/folder/Spam', icon: 'warning'},
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+
+  constructor() {
+  }
+
+  public async doCameraTest() {
+    const cameraPermissions = await Camera.checkPermissions();
+
+    console.log('aaaaaaa', cameraPermissions);
+
+    await Camera.pickImages({
+      quality: 90,
+    });
+  }
 }
