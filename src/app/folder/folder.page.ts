133a151fb941986deea4ec1e41f8ab3a4da64673
Marc Rambow <marc.rambow@sinc.de>
2023-05-10 08:23:38 +0200

    adding camera test stuff

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-folder',
@@ -15,4 +16,14 @@ export class FolderPage implements OnInit {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  request() {
+return Camera.checkPermissions();
+  }
+
+  getPhoto() {
+  return Camera.getPhoto({
+    source: CameraSource.Photos,
+    resultType: CameraResultType.Uri,
+  });
+}
 }
